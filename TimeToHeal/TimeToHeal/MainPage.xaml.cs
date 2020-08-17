cc0501c3ddb329d5cb2e8a0be156edf1b5e03540
Craig Robertson <1109847@rgu.ac.uk>
2020-08-17 12:42:32 +0100

    First Commit

---
+++ TimeToHeal/TimeToHeal/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimeToHeal
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnInfoButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InfoPage());
+        }
+
+        public async void OnChatButtonClicked(Object sender, EventArgs e)
+        {
+           
+        }
+
+        public async void OnHelpButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HelpPage());
+        }
+
+        
+    }
+}
